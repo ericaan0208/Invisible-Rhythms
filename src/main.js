691ae4f1b18fe67dca3383b169249f3541dde8b0
Erica An <erica.an020810@gmail.com>
2026-09-26 00:03:48 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import './pulse.css';
-import './refinement.css';
-import {mountPulse} from './pulse.js';
-const unmount=mountPulse(document.querySelector('#app'));
-addEventListener('pagehide',event=>{if(!event.persisted)unmount();});
